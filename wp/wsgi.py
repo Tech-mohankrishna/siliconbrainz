609ba1764ea2b7388244874709bfff43aa0eba6d
Tech-mohankrishna <mohankrish864@gmail.com>
2020-05-11 00:30:37 +0530

    initial commit

--- wp/wsgi.py
+++ wp/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wp.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wp.settings")
 
 application = get_wsgi_application()
