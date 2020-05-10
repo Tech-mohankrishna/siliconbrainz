290b53ac8e3468e9ae61a14f0134f74bc9897516
Tech-mohankrishna <mohankrish864@gmail.com>
2020-05-10 23:03:09 +0530

    initial commit

--- wp/settings.py
+++ wp/settings.py
@@ -78,6 +78,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'Whitenoise.middleware.WhitenoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'wp.urls'
